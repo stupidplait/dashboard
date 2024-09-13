073e7a9fa01e206c1416a722aab990ff3d3751ff
stupidplait <rusyaplait@gmail.com>
2024-09-13 14:01:00 +0300

    done chapter 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
+import '@/app/ui/global.css';
+import { inter } from './ui/fonts';
+import { ReactNode } from 'react';
+
+interface RootLayoutProps {
+    children: ReactNode,
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
+};
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,37 +1,46 @@
-import {
-  UserGroupIcon,
-  HomeIcon,
-  DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
+'use client';
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
-const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+import { UserGroupIcon, HomeIcon, DocumentDuplicateIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { ForwardRefExoticComponent, RefAttributes, SVGProps } from 'react';
+
+interface LinkInfo {
+    name: string,
+    href: string,
+    icon: ForwardRefExoticComponent<Omit<SVGProps<SVGSVGElement>, "ref"> & {
+        title?: string;
+        titleId?: string;
+    } & RefAttributes<SVGSVGElement>>,
+};
+
+const links: LinkInfo[] = [
+    { name: 'Home', href: '/dashboard', icon: HomeIcon, },
+    { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon, },
+    { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon, },
 ];
 
 export default function NavLinks() {
-  return (
-    <>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
-        return (
-          <a
-            key={link.name}
-            href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </a>
-        );
-      })}
-    </>
-  );
-}
+    const activePath = usePathname();
+
+    return (
+        <>
+            {
+                links.map(({ name, href, icon: Icon }) => (
+                    <Link
+                        key={name}
+                        href={href}
+                        className={clsx(
+                            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                            { 'bg-sky-100 text-blue-600': activePath === href }
+                        )}
+                    >
+                        <Icon className="w-6 transition" />
+                        <p className="hidden transition md:block">{name}</p>
+                    </Link>
+                ))
+            }
+        </>
+    );
+};
